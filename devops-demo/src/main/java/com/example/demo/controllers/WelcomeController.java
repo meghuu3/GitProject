e85a840c6c77cd9a236bce0b86af8c59ff44683f
meghana <user626@host.docker.internal>
2023-10-26 12:15:53 +0530

    commit

---
+++ devops-demo/src/main/java/com/example/demo/controllers/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.services.WelcomeService;
+
+@RestController
+@RequestMapping
+public class WelcomeController {
+	
+	@Autowired
+	private WelcomeService service;
+	
+	public String[] getCity() {
+		return new String[] {"Chennai","pune","Hyderabad"};
+	}
+
+}
