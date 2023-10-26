538094f883d3479c7fd64f65d7fb16ca1edef2c0
meghana <user626@host.docker.internal>
2023-10-26 12:32:51 +0530

    commit new

--- devops-demo/src/main/java/com/example/demo/training/TrainerController.java
+++ devops-demo/src/main/java/com/example/demo/training/TrainerController.java
@@ -1,5 +1,11 @@
 package com.example.demo.training;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TrainerController {
 
+	@GetMapping(path="/bye")
+	public String hello(){
+		return "Message from Trainer";
+	}
 }
